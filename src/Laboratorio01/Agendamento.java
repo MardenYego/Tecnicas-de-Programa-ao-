2684652348fa2e14a90b6da63fe4cfd34cf3dac8
MardenYego <mardenyego@alu.ufc.br>
2022-09-28 21:37:50 -0300

    Segunda alteração

--- src/Laboratorio01/Agendamento.java
+++ src/Laboratorio01/Agendamento.java
@@ -11,4 +11,36 @@ public class Agendamento {
 	private int minuto;
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public int getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+	
+	
 }
