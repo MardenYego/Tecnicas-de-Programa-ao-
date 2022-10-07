69724e6d7ae4054b586ce43fd28cc3078782dddd
MardenYego <mardenyego@alu.ufc.br>
2022-10-07 15:13:06 -0300

    Concluída

--- Lista05/questao03/Circulo.java
+++ Lista05/questao03/Circulo.java
@@ -5,30 +5,32 @@ public class Circulo {
 	private double raio;
 	
 	
-	
-	public void area() {
-		
+	public Circulo(double tamanhoRaio) {
+		setRaio(tamanhoRaio);
 	}
 	
-	public void circunferencia() {
-		
+	public double area() {
+		double area=3.14*Math.pow(raio, 2);
+		return area;
+	}
+	
+	public double circunferencia() {
+		double circunferencia=2*3.14*raio;
+		return circunferencia;
 	}
 	
 	public void aumentarCirculo(double valor) {
-		valor 
+		double aumento= this.raio*valor/100;
+		this.raio+=aumento;
 	}
 	
-	
-	
-	
-	
-
 	public double getRaio() {
 		return raio;
 	}
 
 	public void setRaio(double raio) {
-		this.raio = raio;
+		if(raio>0)
+		   this.raio = raio;
 	}
 	
 }
--- Lista05/questao03/Main.java
+++ Lista05/questao03/Main.java
@@ -1,9 +1,27 @@
 package questao03;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner(System.in);
+		// 1ª parte
+		System.out.println("****Informe o tamanho do raio****");
+		double tamanhoRaio=entrada.nextDouble();
+		Circulo circulo = new Circulo(tamanhoRaio);
+		
+		System.out.println("Área original referente ao raio informado: "+circulo.area());
+		System.out.println("Circunferencia original do círculo informado:"+circulo.circunferencia());
+		
+		
+		
+		System.out.println("\n****Informe a porcentagem de aumento que deseja****");
+		double porcentagem=entrada.nextDouble();
+		circulo.aumentarCirculo(porcentagem);
+		
+		System.out.println("A nova área é :"+circulo.area()+"\nA nova circunferência é :"+circulo.circunferencia());
 
 	}
 
